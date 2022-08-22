6a1626422149dac0a80ccdb6c523a373448339f5
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-22 18:04:34 -0500

    added axios and the url endpoint for the django application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  useEffect(async() => {
+    const response = await axios.get('http://127.0.0.1:8000/music/');
+    console.log(response)
+  }, []);
+
   return (
     <div>
+
     </div>
   );
 }
