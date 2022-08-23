9aa3bfc45262a64bd50bf3745722bd68f8bbc0ab
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-22 22:07:30 -0500

    added brief functionality for the functions of posting a new song and deleting a song. still need to figure out how to add the delete button using the pk, will ask about that later

--- src/App.js
+++ src/App.js
@@ -8,12 +8,25 @@ function App() {
 
   useEffect(() => {
     getAllSongs();
+    postNewSong();
+    deleteSong();
   }, []);
 
   async function getAllSongs(){
     const response = await axios.get('http://127.0.0.1:8000/music/');
     console.log(response.data)
     setSongs(response.data)
+  }
+
+  async function postNewSong(){
+    const response = await axios.post('http://127.0.0.1:8000/music/');
+    console.log(response.data)
+
+  }
+
+  async function deleteSong(){
+    const response = await axios.delete('http://127.0.0.1:8000/music/');
+    console.log(response.data)
   }
 
   return (
