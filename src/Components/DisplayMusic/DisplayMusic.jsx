c6ae09ca0305608dec8055f71aa40b7e30e0aa98
Dan Langan <danlangan@Dans-MBP-2.hsd1.il.comcast.net>
2022-08-23 23:49:47 -0500

    added new .jsx components and coded out the display music file. still need to do imports on it and I need to also have a button for delete but this is just for now.

---
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -0,0 +1,30 @@
+const DisplayMusic = (props) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.parentEntries.map((song) => {
+                    return (
+                    <tr>
+                        <td>{song.title}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.album}</td>
+                        <td>{song.release_date}</td>
+                        <td>{song.genre}</td>
+                    </tr>
+                    );
+                })};
+            </tbody>
+        </table>
+    );
+}
+
+export default DisplayMusic;
